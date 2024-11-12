116c2c7ddd593fde0426ed2e68e53d48db5a5324
03aryan <155942750+03aryan@users.noreply.github.com>
2024-11-12 11:55:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SliderComponent from './SliderComponent';
+
+function App() {
+  return (
+    <div>
+      <SliderComponent />
+    </div>
+  );
+}
+
+export default App;
